1164352a6e2048da24146bdcfd837532f0cdd915
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-22 16:14:28 +0000

    docs(DeepAdd): update.

--- src/lib/deep-add.type.ts
+++ src/lib/deep-add.type.ts
@@ -1,9 +1,17 @@
 /**
- * @description
+ * @description Adds the property to the nested(deep) path, where the path is a string of dot-separated keys.
+ * If the path points to an existing object, it will continue the recursion to add the value at the next level. 
+ * If the path points to a non-existent key, the type will create a new property at that location. 
+ * This allows for deeply nested object manipulation at compile-time, providing a way to safely define nested properties.
  * @export
- * @template {object} Obj 
- * @template {string} Path 
- * @template Value 
+ * @template {object} Obj The type of the object to which the property should be added.
+ * @template {string} Path The path within the object, specified as a dot-separated string.
+ * @template Value The type of the value to be added at the specified path.
+ * @example
+ * type Example1 = DeepAdd<{ user: { name: string } }, 'user.age', number>;
+ * // type Example1 = { user: { name: string; } & { age: number; }; }
+ * type Example2 = DeepAdd<{ user: { address: { city: string } } }, 'user.address.zipcode', string>;
+ * // type Example2 = { user: { address: { city: string; } & { zipcode: string; }; }; }
  */
 export type DeepAdd<
   Obj extends object,
